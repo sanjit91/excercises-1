e38196d3c11a6ddfc08d5ea06c7fcd647b76d60d
Sanjit Kairi <sanjitkoiri9@.com>
2023-01-16 14:19:58 +0530

    there are three question are solved

--- main.go
+++ main.go
@@ -3,16 +3,55 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(evenNumbers([]int{1, 2, 3, 4, 5})) // output: [2, 4]
+
+	// first question output function
+
+	// fmt.Println(evenNumbers([]int{1, 2, 3, 4, 5})) // output: [2, 4]
+
+	// second question output function
+
+	// frequencies := characterFrequency("hello")
+	// fmt.Println(frequencies)
+
+	// third question output function
+
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(sumSlice(nums))
 
 }
 
-func evenNumbers(nums []int) []int {
-	evens := []int{}
+// first question solution
+
+// func evenNumbers(nums []int) []int {
+// 	evens := []int{}
+// 	for _, num := range nums {
+// 		if num%2 == 0 {
+// 			evens = append(evens, num)
+// 		}
+// 	}
+// 	return evens
+// }
+
+// second question solution
+
+// func characterFrequency(s string) map[string]int {
+// 	m := make(map[string]int)
+// 	for _, char := range s {
+// 		if _, ok := m[string(char)]; ok {
+// 			m[string(char)]++
+// 		} else {
+// 			m[string(char)] = 1
+// 		}
+// 	}
+// 	return m
+// }
+
+// third question solution
+
+func sumSlice(nums []int) int {
+	total := 0
 	for _, num := range nums {
-		if num%2 == 0 {
-			evens = append(evens, num)
-		}
+		total += num
 	}
-	return evens
+	return total
 }
